1bab212b1c4d5140d3d7a7a2ed407f91dea87f9f
Mike <bitrise-wr-aaaadfg7hoqhacfs2bivc5ikzi@zepz.org.slack.com>
2024-06-01 19:13:35 +0200

    feat(database): set up initial NestJS modules and PostgreSQL connection
    - Added .env file
    - Configured PostgreSQL connection

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
